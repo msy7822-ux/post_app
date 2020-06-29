9cc1cdba0b3566d3f57be98b8a111698f44c6075
msy7822-ux <msy780022@gmail.com>
2020-06-29 15:48:12 +0900

    add password column

--- dbconnect.php
+++ dbconnect.php
@@ -40,7 +40,7 @@
 
         // DBのテーブルを作成するプログラムを作成
 
-        $sql = 'CREATE TABLE IF NOT EXISTS tbpost (id INT AUTO_INCREMENT PRIMARY KEY, name CHAR(32), comment TEXT, created_at DATETIME);';
+        $sql = 'CREATE TABLE IF NOT EXISTS tbpost (id INT AUTO_INCREMENT PRIMARY KEY, name CHAR(32), comment TEXT, created_at DATETIME, password TEXT);';
 
         $statement = $pdo->query($sql);
     
